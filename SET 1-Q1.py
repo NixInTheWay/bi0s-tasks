b80223129c69c65f6babd56b004bf4671acf5e1d
NixInTheWay <mr.paperbag.00@gmail.com>
2026-08-02 16:23:33 +0530

    Update SET 1-Q1.py

--- SET 1-Q1.py
+++ SET 1-Q1.py
@@ -3,20 +3,17 @@ Encrypted = input("Enter notation: ")
 Decoded = ""
 i=0
 while i>len(Encrypted):
-  try:
-    if Encrypted[i]+Encrypted[i+1] == "xx":
-      Decoded =+ "00"
-      i =+ 2
-    elif Encrypted[i]+Encrypted[i+1] == "ox":
-      Decoded =+ "1"
-      i =+ 2
-    elif Encrypted[i]+Encrypted[i+1] == "oo":
-      Decoded =+ "2"
-      i =+ 2
-    elif Encrypted[i]+Encrypted[i+1] == "xo":
-      i =+ 1
-      Decoded =+ "1"
-  except Exception:
-    pass
+  if Encrypted[i]+Encrypted[i+1] == "xx":
+    Decoded =+ "00"
+    i =+ 2
+  elif Encrypted[i]+Encrypted[i+1] == "ox":
+    Decoded =+ "1"
+    i =+ 2
+  elif Encrypted[i]+Encrypted[i+1] == "oo":
+    Decoded =+ "2"
+    i =+ 2
+  elif Encrypted[i]+Encrypted[i+1] == "xo":
+    i =+ 1
+    Decoded =+ "1"
 print(Decoded)
   
